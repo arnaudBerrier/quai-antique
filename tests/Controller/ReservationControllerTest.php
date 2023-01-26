c3449cfff8d3655ef1704bb6f0ec52d3b8444f72
Arnaud <arnaud91berrier@gmail.com>
2023-01-26 22:53:52 +0100

    Unit test Controller

---
+++ tests/Controller/ReservationControllerTest.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\Reservation;
+use App\Repository\ReservationRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ReservationControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private ReservationRepository $repository;
+    private string $path = '/reservation/';
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->repository = static::getContainer()->get('doctrine')->getRepository(Reservation::class);
+
+        foreach ($this->repository->findAll() as $object) {
+            $this->repository->remove($object, true);
+        }
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', $this->path);
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Reservation index');
+
+        // Use the $crawler to perform additional assertions e.g.
+        // self::assertSame('Some text on the page', $crawler->filter('.p')->first());
+    }
+
+    public function testNew(): void
+    {
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $this->markTestIncomplete();
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Save', [
+            'reservation[dayHourReserv]' => 'Testing',
+            'reservation[nbPeople]' => 'Testing',
+            'reservation[created_at]' => 'Testing',
+            'reservation[client]' => 'Testing',
+        ]);
+
+        self::assertResponseRedirects('/reservation/');
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+    }
+
+    public function testShow(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Reservation();
+        $fixture->setDayHourReserv('My Title');
+        $fixture->setNbPeople('My Title');
+        $fixture->setCreated_at('My Title');
+        $fixture->setClient('My Title');
+
+        $this->repository->save($fixture, true);
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Reservation');
+
+        // Use assertions to check that the properties are properly displayed.
+    }
+
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Reservation();
+        $fixture->setDayHourReserv('My Title');
+        $fixture->setNbPeople('My Title');
+        $fixture->setCreated_at('My Title');
+        $fixture->setClient('My Title');
+
+        $this->repository->save($fixture, true);
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        $this->client->submitForm('Update', [
+            'reservation[dayHourReserv]' => 'Something New',
+            'reservation[nbPeople]' => 'Something New',
+            'reservation[created_at]' => 'Something New',
+            'reservation[client]' => 'Something New',
+        ]);
+
+        self::assertResponseRedirects('/reservation/');
+
+        $fixture = $this->repository->findAll();
+
+        self::assertSame('Something New', $fixture[0]->getDayHourReserv());
+        self::assertSame('Something New', $fixture[0]->getNbPeople());
+        self::assertSame('Something New', $fixture[0]->getCreated_at());
+        self::assertSame('Something New', $fixture[0]->getClient());
+    }
+
+    public function testRemove(): void
+    {
+        $this->markTestIncomplete();
+
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $fixture = new Reservation();
+        $fixture->setDayHourReserv('My Title');
+        $fixture->setNbPeople('My Title');
+        $fixture->setCreated_at('My Title');
+        $fixture->setClient('My Title');
+
+        $this->repository->save($fixture, true);
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->submitForm('Delete');
+
+        self::assertSame($originalNumObjectsInRepository, count($this->repository->findAll()));
+        self::assertResponseRedirects('/reservation/');
+    }
+}
