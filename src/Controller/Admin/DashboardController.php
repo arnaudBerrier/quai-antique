d8b4e9c75ce84138224843a718241aff36315449
Arnaud <arnaud91berrier@gmail.com>
2023-02-03 11:45:27 +0100

    ✨ Establishment Easyadmin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Menu;
+use App\Entity\User;
+use App\Entity\Contact;
+use App\Entity\Category;
+use App\Entity\Products;
+use App\Entity\Restaurant;
+use App\Entity\Reservation;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Quai Antique')
+            ->renderContentMaximized();
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Restaurant', 'fas fa-location-dot', Restaurant::class);
+        yield MenuItem::linkToCrud('Réservation', 'fas fa-shop', Reservation::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Menu', 'fas fa-list-ol', Menu::class);
+        yield MenuItem::linkToCrud('Produits', 'fas fa-map', Products::class);
+        yield MenuItem::linkToCrud('Demande de contact', 'fas fa-envelope', Contact::class);
+    }
+}
